641a08e6586d1089bae45b53fae25f2806772359
Patrick Elsen <pelsen@xfbs.net>
2022-12-30 09:51:50 -0500

    Solves day08 part 2

--- day08-treetop-tree-house/src/main.rs
+++ day08-treetop-tree-house/src/main.rs
@@ -76,6 +76,95 @@ fn solve(data: &Vec<Vec<u8>>) -> usize {
     visible(data).len()
 }
 
+fn scenic_score_right(row: &[u8], pos: usize) -> u64 {
+    assert!(pos <= row.len());
+    let trees = row
+        .into_iter()
+        .skip(pos + 1)
+        .take_while(|e| **e < row[pos])
+        .count();
+    let trees = (trees + 1).min(row.len() - pos - 1);
+    trees as u64
+}
+
+#[test]
+fn can_compute_scenic_score_right() {
+    assert_eq!(scenic_score_right(&[2, 5, 5, 1, 2], 0), 1);
+    assert_eq!(scenic_score_right(&[2, 5, 5, 1, 2], 1), 1);
+    assert_eq!(scenic_score_right(&[2, 5, 5, 1, 2], 2), 2);
+    assert_eq!(scenic_score_right(&[2, 5, 5, 1, 2], 3), 1);
+    assert_eq!(scenic_score_right(&[2, 5, 5, 1, 2], 4), 0);
+    assert_eq!(scenic_score_right(&[3, 3, 5, 4, 9], 0), 1);
+    assert_eq!(scenic_score_right(&[3, 3, 5, 4, 9], 1), 1);
+    assert_eq!(scenic_score_right(&[3, 3, 5, 4, 9], 2), 2);
+    assert_eq!(scenic_score_right(&[3, 3, 5, 4, 9], 3), 1);
+    assert_eq!(scenic_score_right(&[3, 3, 5, 4, 9], 4), 0);
+}
+
+fn scenic_score_left(row: &[u8], pos: usize) -> u64 {
+    assert!(pos <= row.len());
+    let trees = row
+        .into_iter()
+        .rev()
+        .skip(row.len() - pos)
+        .take_while(|e| **e < row[pos])
+        .count();
+    (trees + 1).min(pos) as u64
+}
+
+#[test]
+fn can_compute_scenic_score_left() {
+    assert_eq!(scenic_score_left(&[2, 5, 5, 1, 2], 0), 0);
+    assert_eq!(scenic_score_left(&[2, 5, 5, 1, 2], 1), 1);
+    assert_eq!(scenic_score_left(&[2, 5, 5, 1, 2], 2), 1);
+    assert_eq!(scenic_score_left(&[2, 5, 5, 1, 2], 3), 1);
+    assert_eq!(scenic_score_left(&[2, 5, 5, 1, 2], 4), 2);
+    assert_eq!(scenic_score_left(&[3, 3, 5, 4, 9], 0), 0);
+    assert_eq!(scenic_score_left(&[3, 3, 5, 4, 9], 1), 1);
+    assert_eq!(scenic_score_left(&[3, 3, 5, 4, 9], 2), 2);
+    assert_eq!(scenic_score_left(&[3, 3, 5, 4, 9], 3), 1);
+    assert_eq!(scenic_score_left(&[3, 3, 5, 4, 9], 4), 4);
+}
+
+fn scenic_score(row: &[u8], col: &[u8], x: usize, y: usize) -> u64 {
+    assert!(x < row.len());
+    assert!(y < col.len());
+    assert_eq!(row[x], col[y]);
+
+    [
+        scenic_score_left(row, x),
+        scenic_score_right(row, x),
+        scenic_score_left(col, y),
+        scenic_score_right(col, y),
+    ]
+    .into_iter()
+    .product()
+}
+
+#[test]
+fn can_compute_scenic_score() {
+    assert_eq!(scenic_score(&[2, 5, 5, 1, 2], &[3, 5, 3, 5, 3], 2, 1), 4);
+    assert_eq!(scenic_score(&[3, 3, 5, 4, 9], &[3, 5, 3, 5, 3], 2, 3), 8);
+}
+
+fn solve_two(data: &Vec<Vec<u8>>) -> u64 {
+    let transposed = transpose(data);
+    let (_x, _y, score) = data
+        .iter()
+        .enumerate()
+        .map(|(y, row)| {
+            transposed
+                .iter()
+                .enumerate()
+                .map(|(x, col)| (x, y, scenic_score(&row, &col, x, y)))
+                .max_by_key(|(_, _, score)| *score)
+                .unwrap()
+        })
+        .max_by_key(|(_, _, score)| *score)
+        .unwrap();
+    score
+}
+
 #[test]
 fn can_check_visible_left() {
     assert_eq!(visible_left(&[3, 0, 3, 7, 3]), vec![0, 3]);
@@ -122,10 +211,19 @@ fn can_transpose() {
     );
 }
 
+#[test]
+fn can_solve_two() {
+    let data = parse(include_str!("../example.txt"));
+    let score = solve_two(&data);
+    assert_eq!(score, 8);
+}
+
 fn main() {
     let file = std::env::args().nth(1).unwrap();
     let data = std::fs::read_to_string(file).unwrap();
     let data = parse(&data);
     let result = solve(&data);
     println!("{result}");
+    let score = solve_two(&data);
+    println!("{score}");
 }
